6e89316d83df912694311c0981fd97e6a0bf9014
Sergey <Sergeev_sergey99@mail.ru>
2019-12-23 19:35:49 +0000

    add sfml

---
+++ LIFE_SFML.cpp
@@ -0,0 +1,145 @@
+//#include <iostream>
+//Необходимо для новых команд
+//#include <windows.h>
+
+#include <SFML/Graphics.hpp>
+//using namespace std;
+//колличество элементов по горизонтали
+const unsigned int X = 180;
+//колличество элементов по вертикали
+const unsigned int Y = 90;
+//размер клетки
+const unsigned int H = 10;
+// Массив элементов одного кадра
+int lifenow[X][Y] = {};
+int lifenext[X][Y] = {};
+
+
+sf::RenderWindow window(sf::VideoMode((X * H), (Y * H)), "LIFE");
+
+void Draw()
+{
+	// Очистка экрана
+//	system("cls");
+	//Построчный вывод слоев кадра
+	for (int i = 0; i < Y; i++)
+	{
+		//Посимвольный вывод одного  слоя кадра
+		for (int j = 0; j < X; j++)
+		{
+			sf::RectangleShape rectangle(sf::Vector2f(H, H));
+			if (lifenow[j][i] == 0)
+				rectangle.setFillColor(sf::Color(0, 0, 0));
+			else
+				rectangle.setFillColor(sf::Color(0, 100, 0));
+			rectangle.setPosition(j * H, i * H);
+			window.draw(rectangle);
+//			cout << lifenow[j][i];
+		}
+		// Перевод на новую строку
+		// Чтобы не вывести кадр одной строчкой
+//		cout << endl;
+	}
+}
+void Fill()
+{
+	for (int i = 0; i < Y; i++)
+	{
+		for (int j = 0; j < X; j++)
+		{
+			lifenow[j][i] = ((rand() % 3 == 0)?'O':0);
+		}
+	}
+}
+/*bool Keys()
+{	
+	// Функция GetKeyState дает много информации о клавише
+	// Так как необходимо только узнать, нажата ли она?
+	// Необходимо сделать СДВИГ вправо на 7 символов с помощью >> 
+	if (GetKeyState(27) >> 7) // Если нажата клавиша
+		// с кодом 27 (ESC), то возвращаем ложь 
+		return false;
+	if (GetKeyState(' ') >> 7)
+		Fill();
+	return true;
+}*/
+int countOfOther(int i, int j)
+{
+	int cnt = 0;
+	//if (lifenow[(i) % X][(j) % Y] != 0) cnt++;
+	if (lifenow[(i - 1) % X][(j) % Y] != 0) cnt++;
+	if (lifenow[(i + 1) % X][(j) % Y] != 0) cnt++;
+	if (lifenow[(i) % X][(j - 1) % Y] != 0) cnt++;
+	if (lifenow[(i - 1) % X][(j - 1) % Y] != 0) cnt++;
+	if (lifenow[(i + 1) % X][(j - 1) % Y] != 0) cnt++;
+	if (lifenow[(i) % X][(j + 1) % Y] != 0) cnt++;
+	if (lifenow[(i - 1) % X][(j + 1) % Y] != 0) cnt++;
+	if (lifenow[(i + 1) % X][(j + 1) % Y] != 0) cnt++;
+	return cnt;
+}
+void Swap()
+{
+	for (int i = 0; i < Y; i++)
+	{
+		for (int j = 0; j < X; j++)
+		{
+			lifenow[j][i] = lifenext[j][i];
+		}
+	}
+}
+void Update()
+{
+	for (int i = 0; i < Y; i++)
+	{
+		for (int j = 0; j < X; j++)
+		{
+			int cnt = countOfOther(j, i);
+			if (cnt < 2 || cnt > 3) lifenext[j][i] = 0;
+			if (lifenow[j][i] == 0 && cnt == 3) lifenext[j][i] = 'O';
+			if (lifenow[j][i] == 'O' && (cnt == 3 || cnt == 2)) lifenext[j][i] = 'O';
+		}
+
+	}
+	Swap();
+}
+int main()
+{
+	srand(unsigned(0));
+	window.setFramerateLimit(60);
+
+	sf::Event event;
+	while (window.isOpen())
+	{
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed) window.close();
+			if (event.type == sf::Event::KeyPressed)
+			{
+				// Получаем нажатую клавишу - выполняем соответствующее действие
+				if (event.key.code == sf::Keyboard::Escape) window.close();
+				if (event.key.code == sf::Keyboard::Space) Fill();
+			}
+		}
+
+
+
+		// Выполняем необходимые действия по отрисовке
+		window.clear();
+		
+
+		Update();
+		Draw();
+		window.display();
+	}
+	/*bool GameIsRun = true;
+	while (GameIsRun) // Повтор пока GameIsRun == true
+	{
+		GameIsRun = Keys(); // Если нажата 
+		// клавиша выхода ESC, то GameIsRun = false
+		Update(); // Подготовка кадра
+		Draw(); // Отрисовка кадра
+		Sleep(60); // Задержка между кадрами
+	}*/
+	return 0;
+}
+
