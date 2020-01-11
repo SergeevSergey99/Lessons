f9e2de698468ef6b90cd3d78af59ef2d5bf9d0ba
Sergey <Sergeev_sergey99@mail.ru>
2020-01-11 10:15:04 +0000

    add pointers

---
+++ Pointer.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int number = 42;
+	// Тип * Имя
+	int * pointer;
+	// Запись в указатель адреса переменной number
+	pointer = &number;
+
+	// Вывод значения переменной number
+	cout << "Значение number: " << number << endl;
+	// Вывод адреса переменной number
+	cout << "Адрес number: " << &number << endl;
+
+	// Вывод адреса на который ссылается указатель
+	cout << "Указатель pointer ссылается на адрес: " << pointer << endl;
+	// Вывод значения переменной на адрес который ссылается указатель
+	cout << "Указатель pointer ссылается на переменную со значением: " << *pointer << endl;
+}
