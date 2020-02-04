99cf25b9138b55382ebc7523bf53ea6f8d483832
Sergey <Sergeev_sergey99@mail.ru>
2020-02-04 00:50:05 +0000

    add struct

---
+++ Struct.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+using namespace std;
+
+// Объединяем несколько переменных в структуру
+struct Person
+{
+	string name;
+	string lastName;
+	string phone;
+	int age;
+};
+
+// Функция создающаяя персону с заданными значениями
+Person SetStandartPerson()
+{
+	// Выделяем память для элемента структуры 
+	Person person;
+	// Чтобы изменить значения в структуре
+	// указываем через точку что нужно изменить
+	person.lastName = "Иванов";
+	person.name = "Иван";
+	person.phone = "+7(999)-999-99-99";
+	person.age = 42;
+	// Вместо Функции будет поставлена эта персона
+	return person;
+}
+
+// Функция вывода на экран данных, хранящихся в структуре персоны
+void PrintPerson(Person person)
+{
+	cout << endl;
+	cout << "Имя: " << person.lastName << " " << person.name << endl;
+	cout << "Возраст: " << person.age << endl;
+	cout << "Телефон: " << person.phone << endl << endl;
+}
+
+int main()
+{
+	// Выделяем память для элемента структуры 
+	Person person1;
+	// Задаем данные у персоны вручную
+	person1.name = "Петр";
+	person1.lastName = "Петров";
+	person1.age = 0;
+	person1.phone = "+7(000)-000-00-00";
+	// Печатаем person1
+	PrintPerson(person1);
+
+	// Выделяем память для элемента структуры 
+	// Задаем данные у персоны по шаблону
+	Person person2 = SetStandartPerson();
+	// Печатаем person2
+	PrintPerson(person2);
+}
